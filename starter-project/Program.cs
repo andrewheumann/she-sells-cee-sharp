f2b24358e0979cc25e1d2758713d68ca55faa336
Andrew Heumann <andrewheumann@Andrews-MacBook-Pro-2.local>
2018-03-30 12:32:26 -0400

    more example dev

--- starter-project/Program.cs
+++ starter-project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace starterproject
@@ -15,9 +17,38 @@ namespace starterproject
                 HeightInches = 6
             };
 
+            var workers = GenerateWeWorkers();
+
+            var json = JsonConvert.SerializeObject(workers);
+
+            File.WriteAllText("workers.json", json);
+
+            Console.WriteLine(json);
 
             Console.WriteLine($"{andrew.Name} is {andrew.Age} years old");
             Console.WriteLine($"{andrew.Name} is {andrew.HeightFormatted()} tall");
+        }
+
+        public static List<WeWorker> GenerateWeWorkers()
+        {
+            var workers = new List<WeWorker>();
+            workers.Add(
+                new WeWorker
+                {
+                    Name = "Andrew",
+                    Age = 29,
+                    HeightFeet = 5,
+                    HeightInches = 6
+                });
+            workers.Add(
+                new WeWorker
+                {
+                    Name = "Bob",
+                    Age = 27,
+                    HeightFeet = 5,
+                    HeightInches = 10
+                });
+            return workers;
         }
     }
 }
