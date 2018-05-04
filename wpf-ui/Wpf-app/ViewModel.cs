8a03cce61e694043a927b652df7e9c7833547fef
Andrew Heumann <andheum@gmail.com>
2018-05-04 09:36:57 -0400

    WPF example start

---
+++ wpf-ui/Wpf-app/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_app
+{
+    public class ViewModel
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+
+    public static class ViewModelSampleDataContext
+    {
+        public static ViewModel Model => new ViewModel
+        {
+            Name = "HELLO!",
+            Value = 12
+        };
+    }
+}
